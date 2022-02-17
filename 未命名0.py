5f4c74c5461eb2553024cfc52f62bcb31a42e3bb
sunhaha123 <Sun78998779>
2022-02-17 19:46:16 +0800

    firtst

---
+++ 未命名0.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 18:35:28 2022
+
+@author: Administrator
+"""
+
+from mathutils import Matrix, Vector, Quaternion
+from math import radians
+import math
+import numpy as  np
+
+def Rodrigues(self,rotvec):
+    """
+        欧拉角转为旋转矩阵 等同于 mathutils.Matrix.Rotation()
+    """
+    theta = np.linalg.norm(rotvec)
+    r = (rotvec/theta).reshape(3, 1) if theta > 0. else rotvec
+    cost = np.cos(theta)
+    mat = np.asarray([[0, -r[2], r[1]],
+                      [r[2], 0, -r[0]],
+                      [-r[1], r[0], 0]])
+    return(cost*np.eye(3) + (1-cost)*r.dot(r.T) + np.sin(theta)*mat)
+
+def EulerAndQuaternionTransform(intput_data):
+    """
+        四元素与欧拉角互换
+    """
+    data_len = len(intput_data)
+    angle_is_not_rad = False
+ 
+    if data_len == 3:
+        r = 0
+        p = 0
+        y = 0
+        if angle_is_not_rad: # 180 ->pi
+            r = math.radians(intput_data[0]) 
+            p = math.radians(intput_data[1])
+            y = math.radians(intput_data[2])
+        else:
+            r = intput_data[0] 
+            p = intput_data[1]
+            y = intput_data[2]
+ 
+        sinp = math.sin(p/2)
+        siny = math.sin(y/2)
+        sinr = math.sin(r/2)
+ 
+        cosp = math.cos(p/2)
+        cosy = math.cos(y/2)
+        cosr = math.cos(r/2)
+ 
+        w = cosr*cosp*cosy + sinr*sinp*siny
+        x = sinr*cosp*cosy - cosr*sinp*siny
+        y = cosr*sinp*cosy + sinr*cosp*siny
+        z = cosr*cosp*siny - sinr*sinp*cosy
+        return [w,x,y,z]
+ 
+    elif data_len == 4:
+ 
+        w = intput_data[0] 
+        x = intput_data[1]
+        y = intput_data[2]
+        z = intput_data[3]
+ 
+        r = math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y))
+        p = math.asin(2 * (w * y - z * x))
+        y = math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z))
+ 
+        if angle_is_not_rad : # pi -> 180
+            r = math.degrees(r)
+            p = math.degrees(p)
+            y = math.degrees(y)
+        return [r,p,y]
+
+def quaternion_to_rotation_matrix(q):  # x, y ,z ,w
+    """
+        四元数转为旋转矩阵
+    """
+    rot_matrix = np.mat(
+        [[1.0 - 2 * (q[1] * q[1] + q[2] * q[2]), 2 * (q[0] * q[1] - q[3] * q[2]), 2 * (q[3] * q[1] + q[0] * q[2])],
+         [2 * (q[0] * q[1] + q[3] * q[2]), 1.0 - 2 * (q[0] * q[0] + q[2] * q[2]), 2 * (q[1] * q[2] - q[3] * q[0])],
+         [2 * (q[0] * q[2] - q[3] * q[1]), 2 * (q[1] * q[2] + q[3] * q[0]), 1.0 - 2 * (q[0] * q[0] + q[1] * q[1])]],
+        )
+    return rot_matrix
+
+
+
+quat_x_90_cw = Quaternion((1.0, 0.0, 0.0), radians(90))
+
+#四元数
+weizhi = [0.7071068286895752, 0.7071067690849304, 0.0, 0.0]
+#四元数转为旋转矩阵
+quaternion_to_rotation_matrix(weizhi)
+
+new_matrix = Rodrigues([radians(90),0,0])
+
+r,p,y = EulerAndQuaternionTransform(weizhi)
+
